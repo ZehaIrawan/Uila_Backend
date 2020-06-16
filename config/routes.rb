8429d7268902155165ef81fbe4941ebab2696ace
Zeha Irawan <jangkaringbumi@gmail.com>
2020-06-16 17:34:48 +0700

    experimenting with admin auth for creating product

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
       get 'post/index'
       post :auth, to: 'authentication#create'
       get  '/auth' => 'authentication#fetch'
+      get 'products/index'
+      resources :products
     end
   end
 end
