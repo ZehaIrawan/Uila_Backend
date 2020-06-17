28afb0771f1dbc241d4a289d6d0ced5a6a84a668
Zeha Irawan <jangkaringbumi@gmail.com>
2020-06-17 14:23:07 +0700

    add show & destroy method for product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::ProductsController < ApiController
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, :only => [:index]
+  skip_before_action :authenticate_user!, :only => [:index,:show]
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
 def index
     render json: { product: Product.all }
+end
+
+def show
+  @product = Product.find(params[:id])
+  render json: @product
 end
 
 def create
@@ -29,9 +34,17 @@ def update
   else
     render json:{errors:'You are not authorized'}
   end
-
 end
 
+  def destroy
+    if current_user.admin?
+      @product = Product.find(params[:id])
+      @product.destroy!
+      render json:{status:'Product has been deleted'}
+    else
+      render json:{errors:'You are not authorized'}
+    end
+  end
 
 private
   def product_params
