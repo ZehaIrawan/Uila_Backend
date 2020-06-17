f46e4892046f4c09a3c9b8d196f1f9f04647d248
Zeha Irawan <jangkaringbumi@gmail.com>
2020-06-17 12:20:00 +0700

    add only admin update method for product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ProductsController < ApiController
   before_action :authenticate_user!
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
 def index
     puts current_user.email
@@ -20,9 +21,25 @@ def create
   end
 end
 
+def update
+  if current_user.admin?
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    render json:@product
+  else
+    render json:{errors:'You are not authorized'}
+  end
+
+end
+
 
 private
-def product_params
+  def product_params
     params.permit(:title, :description, :price)
   end
+
+  def record_not_found(error)
+    render json: { error: error.message }, status: :not_found
+  end
+
 end
